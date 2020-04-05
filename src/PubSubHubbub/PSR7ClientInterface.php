9799ddff8cb9a4198beb970927128268cbba6b46
Aurimas R <mail@aurimas.eu>
2020-04-05 19:01:55 +0900

    Alignment to PSR18 and PSR17

--- src/PubSubHubbub/PSR7ClientInterface.php
+++ src/PubSubHubbub/PSR7ClientInterface.php
@@ -2,33 +2,11 @@
 
 namespace Laminas\Feed\PubSubHubbub;
 
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
-interface PSR7ClientInterface
+interface PSR7ClientInterface extends RequestFactoryInterface, ClientInterface
 {
-    /**
-     * Sends a PSR-7 request and returns a PSR-7 response.
-     *
-     * @param RequestInterface $request
-     *
-     * @return ResponseInterface
-     *
-     */
-    public function sendRequest(RequestInterface $request): ResponseInterface;
-
-    /**
-     * @param string                               $method  HTTP method
-     * @param string|UriInterface                  $uri     URI
-     * @param array                                $headers Request headers
-     * @param string|null|resource|StreamInterface $body    Request body
-     * @param string                               $version Protocol version
-     */
-    public function createRequest(
-        $method,
-        $uri,
-        array $headers = [],
-        $body = null,
-        $version = '1.1'
-    ): RequestInterface;
 }
